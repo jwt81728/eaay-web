6dd7830e05f9c2289cf1ac6ce6f57b9c896241a8
chun yen <jwt81728@yahoo.com.tw>
2020-10-13 11:20:44 +0800

    First Deploy

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ app=Flask(__name__) # __name__ 代表目前執行的模組
 
 @app.route("/") # 函式的裝飾 (Decorator) :以函式為基礎,提供附加的功能
 def home():
-    return "Hello Flask"
+    return "Hello 廢物! 今天吃什麼?!"
 @app.route("/test")
 
 def test():
-    return "小威嫩嫩"
+    return "小威嫩嫩還在睡阿? 吃鍋貼好了!"
 if __name__=="__main__": # 如果以主程式執行
     app.run() # 立刻啟動伺服器
