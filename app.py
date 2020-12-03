181c73c8e582bdabe1c0fd9ef647f858c146e110
chun yen <jwt81728@yahoo.com.tw>
2020-12-03 11:00:52 +0800

    upda2

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-app=Flask(__name__) # __name__ 代表目前執行的模組
+app=Flask(__name__, static_url_path='/static',static_folder='./static') # __name__ 代表目前執行的模組
 
 @app.route("/") # 函式的裝飾 (Decorator) :以函式為基礎,提供附加的功能
 def home():
@@ -21,7 +21,6 @@ def shu():
     return "             我是ㄎ一ㄤ妹, 劉緒緒!!!"
 
 
-app = Flask(__name__, static_url_path='/static',static_folder='./static')
 
 @app.route('/pic')
 def images():
